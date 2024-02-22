356deaab9d4739daaa1fcd7f6583c8be6a6d35ea
ShubhChaturvedi <chaturvedishubh650@gmail.com>
2024-02-22 14:33:55 +0530

    DEV : Whole project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,112 @@
+import Link from 'next/link'
+import MaxWidthWrapper from './MaxWidthWrapper'
+import { Icons } from './Icons'
+import NavItems from './NavItems'
+import { buttonVariants } from './ui/button'
+import Cart from './Cart'
+import { getServerSideUser } from '@/lib/payload-utils'
+import { cookies } from 'next/headers'
+import UserAccountNav from './UserAccountNav'
+import MobileNav from './MobileNav'
+import { MessageIcon } from './ui/message-icon'
+
+const Navbar = async () => {
+  const nextCookies = cookies()
+  const { user } = await getServerSideUser(nextCookies)
+
+  return (
+    <div className='bg-white sticky z-50 top-0 inset-x-0 h-16'>
+      <header className='relative bg-white'>
+        <MaxWidthWrapper>
+          <div className='border-b border-gray-200'>
+            <div className='flex h-16 items-center'>
+              <MobileNav />
+
+              <div className='ml-4 flex lg:ml-0'>
+                <Link href='/'>
+                  <Icons.logo className='h-10 w-10' />
+                </Link>
+              </div>
+
+              {/* button for help as a link */}
+
+              <div className='hidden lg:flex lg:ml-8 lg:space-x-6'>
+                <Link
+                target='_blank'
+                  href='http://127.0.0.1:5000'
+                  className={buttonVariants({
+                    variant: 'ghost',
+                  })}>
+                  Help
+                </Link>
+              </div>
+
+              <div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
+                <NavItems />
+              </div>
+
+              <div className='ml-auto flex items-center'>
+                <div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
+                  {user ? null : (
+                    <Link
+                      href='/sign-in'
+                      className={buttonVariants({
+                        variant: 'ghost',
+                      })}>
+                      Sign in
+                    </Link>
+                  )}
+
+                  {user ? null : (
+                    <span
+                      className='h-6 w-px bg-gray-200'
+                      aria-hidden='true'
+                    />
+                  )}
+
+                  {user ? (
+                    <UserAccountNav user={user} />
+                  ) : (
+                    <Link
+                      href='/sign-up'
+                      className={buttonVariants({
+                        variant: 'ghost',
+                      })}>
+                      Create account
+                    </Link>
+                  )}
+
+                  {user ? (
+                    <span
+                      className='h-6 w-px bg-gray-200'
+                      aria-hidden='true'
+                    />
+                  ) : null}
+
+                  {user ? null : (
+                    <div className='flex lg:ml-6'>
+                      <span
+                        className='h-6 w-px bg-gray-200'
+                        aria-hidden='true'
+                      />
+                    </div>
+                  )}
+
+                  <div className='ml-4 flow-root lg:ml-6'>
+                    <Cart />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </header>
+      {/* pos absolute and general property */}
+      {/* <div className='absolute top-0 right-0 flex items-center justify-center h-6 w-6'>
+        <MessageIcon />
+        </div> */}
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/components/ui/message-icon.tsx
@@ -0,0 +1,36 @@
+// "use client";
+
+// import * as React from "react";
+// import { cva, type VariantProps } from "class-variance-authority";
+
+// import { cn } from "@/lib/utils";
+
+// const messageIconVariants = cva(
+//   "w-5 h-5 text-gray-500 peer-disabled:text-gray-300"
+// );
+
+// export const MessageIcon = ({
+//     className,
+//     variant,
+// }: VariantProps<typeof messageIconVariants>) => {
+//     const classes = messageIconVariants(variant);
+
+//   return (
+//     <svg
+//       xmlns="http://www.w3.org/2000/svg"
+//       className={cn(classes, className)}
+//       fill="none"
+//       viewBox="0 0 24 24"
+//       stroke="currentColor"
+//     >
+//       <path
+//         strokeLinecap="round"
+//         strokeLinejoin="round"
+//         strokeWidth={2}
+//         d="M12 16l4 4m0 0l4-4m-4 4V4"
+//       />
+//     </svg>
+//   );
+// };
+
+// export default MessageIcon;
